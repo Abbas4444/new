92b4214c19e13be253474d0e170d34ec063165d1
Abbas4444 <alakdawala11@gmail.com>
2016-07-19 19:29:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <body data-spy="scroll" data-target="#my-navbar">
 
   <!-- Navbar -->
-  	<nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
+  	<nav class="navbar navbar-inverse " id="my-navbar">
   		<div class="container">
   			<div class="navbar-header">
   				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
